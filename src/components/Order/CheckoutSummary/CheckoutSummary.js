b6326171b9fd73cbae64b4f551f9765f1ccd61f8
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-05-03 22:11:43 +0300

    Navigating back & to next page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -11,10 +11,12 @@ const checkoutSummary = (props) => {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked>
+      <Button btnType="Danger" clicked={props.checkoutCancelled}>
         CANCEL
       </Button>
-      <Button btnType="Success">CONTINUE</Button>
+      <Button btnType="Success" clicked={props.checkoutContinued}>
+        CONTINUE
+      </Button>
     </div>
   )
 }
