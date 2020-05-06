8906e454bd820cf18de5b2ca8b47a2b0d9c12877
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-05-07 02:28:42 +0300

    Outputting the orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,12 +14,11 @@ class Orders extends Component {
     axios
       .get('/orders.json')
       .then((res) => {
-        console.log(res.data)
         const fetchedOrders = []
         for (let key in res.data) {
           fetchedOrders.push({ ...res.data[key], id: key })
         }
-        this.setState({ loading: false })
+        this.setState({ loading: false, orders: fetchedOrders })
       })
       .catch((err) => {
         this.setState({ loading: false })
@@ -29,8 +28,13 @@ class Orders extends Component {
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map((order) => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
       </div>
     )
   }
