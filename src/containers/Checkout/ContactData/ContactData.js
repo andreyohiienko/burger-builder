0bd7840bc657ff9918a455913cf115607e29f9c5
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-05-18 02:16:24 +0300

    Connecting contact data container & actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders'
 import Input from '../../../components/UI/Input/Input'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions'
 
 class ContactData extends Component {
   state = {
@@ -126,7 +128,6 @@ class ContactData extends Component {
 
   orderHandler = (event) => {
     event.preventDefault()
-    this.setState({ loading: true })
     const formData = {}
     for (let formElementIdentifier in this.state.orderForm) {
       formData[formElementIdentifier] = this.state.orderForm[
@@ -138,15 +139,7 @@ class ContactData extends Component {
       price: this.props.price, // should be calculated on a server in order to prevent customer editing
       orderData: formData,
     }
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false })
-        this.props.history.push('/')
-      })
-      .catch((error) => {
-        this.setState({ loading: false })
-      })
+    this.props.onOrderBurger(order)
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -219,4 +212,14 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(ContactData)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onOrderBurger: (orderData) =>
+      dispatch(actions.purchaseBurgerStart(orderData)),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withErrorHandler(ContactData, axios))
