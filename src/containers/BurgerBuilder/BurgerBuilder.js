4f65060faaf2a9762c5cf030542c069469156819
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-05-26 19:09:45 +0300

    Redirecting the user to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,6 +41,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({ purchasing: true })
     } else {
+      this.props.onSetAuthRedirectPath('/checkout')
       this.props.history.push('/auth')
     }
   }
@@ -127,6 +128,8 @@ const mapDispatchToProps = (dispatch) => {
       return dispatch(actions.initIngredients())
     },
     onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) =>
+      dispatch(actions.setAuthRedirectPath(path)),
   }
 }
 
